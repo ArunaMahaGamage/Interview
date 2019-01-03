4135c7c75b3e6a2e24a7e0296fb2f745bd0a41e0
Aruna <admgbit@gmail.com>
2019-01-03 14:21:04 +0530

    Added GridView

--- app/src/main/java/com/example/aruna/interview/LoginActivity.java
+++ app/src/main/java/com/example/aruna/interview/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example.aruna.interview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,19 @@ public class LoginActivity extends AppCompatActivity {
 
         //Hide Action Bar
         getSupportActionBar().hide();
+
+        initUI();
+    }
+
+    private void initUI() {
+        btn_login = findViewById(R.id.btn_login);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), GridViewExampleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
